11cdab45c849cd3294591b0e75d5af11a55d4b98
PedroBarge <pedro525tavares@gmail.com>
2024-01-08 13:47:15 +0000

    idk

--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,17 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Server {
+
+    public Server() {
+    }
+
+    public void turnOnServer() throws IOException {
+        try {
+            ServerSocket serverSocket = new ServerSocket(9999);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
