8d10d8d9dd66b4034fdb6072fe986bee42af95da
Bruna Taketsuma <mindera@ME00393.local>
2024-01-09 12:44:51 +0000

    client

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Client {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+    private BufferedReader reader;
+    private PrintWriter writer;
+    private Socket socketClient;
+
+
+    private void comunicateClient() throws IOException {
+    writer = new PrintWriter(socketClient.getOutputStream(), true);   //limpar+escrever
+    reader = new BufferedReader(new InputStreamReader(socketClient.getInputStream()));  //ler
+}
+
 }
--- src/ClientManager.java
+++ src/ClientManager.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class ClientManager {
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class ClientManager {
+    private BufferedReader reader;
+    private PrintWriter writer;
+    private Socket socketClient;
+
+    private void createClient() {
+        try {
+            Socket socket = new Socket();
+            System.out.println("create user: ");
+        }catch (Exception e){
+
+        }
+    }
+
+    private void connectServer(InetAddress ip, int port) throws IOException {
+        socketClient = new Socket(ip, port);
+    }
+
+
+    private void saveClient(){
+
+    }
+
+
 }
